033b47601d754c94b39d8f09c273026dd713f97a
Peter-DBRE <60429614+Peter-DBRE@users.noreply.github.com>
2020-01-30 15:49:03 +0000

    Update process_a_All.go

--- cmd/xelogstash/process_a_All.go
+++ cmd/xelogstash/process_a_All.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/billgraziano/Peter-DBRE/config"
+	"github.com/Peter-DBRE/xelogstash/config"
 	humanize "github.com/dustin/go-humanize"
 	"github.com/dustin/go-humanize/english"
 	log "github.com/sirupsen/logrus"
